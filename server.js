4cf5d59bfa9ce07051f592ce8565ef3d5a7f0a43
Rubinel Youan Bi <you18034@byui.edu>
2025-02-07 15:57:39 -0700

    Intro to routing practice

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 import express from "express";
+import { fileURLToPath } from "url";
+import path from "path";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const name = process.env.NAME; // <-- NEW
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/page1", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/page1.html"));
+});
+
+app.get("/page2", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/page2.html"));
+});
 
 app.get("/", (req, res) => {
   res.send(`Hello, ${name}!`); // <-- UPDATED
@@ -19,7 +38,7 @@ app.get("/about", (req, res) => {
 app.get("/Home", (req, res) => {
   res.send("This is the Home page!");
 });
-const PORT = 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
